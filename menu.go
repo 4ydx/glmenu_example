f937168763c85291dfb2e5221dcfc4e4607b2bf3
nate <nathan@4ydx.com>
2014-11-18 11:27:47 +0900

    Example of centered options.

--- menu.go
+++ menu.go
@@ -6,11 +6,10 @@ import (
 )
 
 func menuInit(window *glfw.Window) (err error) {
-	margin := float32(100)
 	fontScale := int32(25)
 	width, height := window.GetSize()
 	menu.Load(float32(width), float32(height), fontScale)
-	menu.Font.SetTextLowerBound(0.5)
+	menu.Font.SetTextLowerBound(0.3)
 	menu.ResizeWindow(float32(width), float32(height))
 
 	//2DO: sounds
@@ -19,9 +18,7 @@ func menuInit(window *glfw.Window) (err error) {
 	var label glmenu.Label
 	menu.AddLabel(&label, "Start")
 
-	label.AddShadow(3, 0, 0, 0)
-	label.Text.SetPosition(0, menu.Height-margin)
-
+	label.AddShadow(1.5, 0, 0, 0)
 	label.Text.SetColor(0.5, 0.5, 0.5, 1)
 	label.OnClick = func(label *glmenu.Label, xPos, yPos float64) (err error) {
 		menu.Toggle()
@@ -35,14 +32,12 @@ func menuInit(window *glfw.Window) (err error) {
 		label.Text.AddScale(-menu.TextScaleRate)
 		return
 	}
-	label.UpdateShadow(3, 0, 0, 0)
 
 	// options
 	var label2 glmenu.Label
 	menu.AddLabel(&label2, "Options")
 
-	label2.AddShadow(3, 0, 0, 0)
-	label2.Text.SetPosition(0, label.Text.X1.Y)
+	label2.AddShadow(1.5, 0, 0, 0)
 	label2.Text.SetColor(0.5, 0.5, 0.5, 1)
 	label2.OnClick = func(label *glmenu.Label, xPos, yPos float64) (err error) {
 		// 2DO: show another menu
@@ -56,14 +51,12 @@ func menuInit(window *glfw.Window) (err error) {
 		label.Text.AddScale(-menu.TextScaleRate)
 		return
 	}
-	label2.UpdateShadow(3, 0, 0, 0)
 
 	// quit
 	var label3 glmenu.Label
 	menu.AddLabel(&label3, "Quit")
 
-	label3.AddShadow(3, 0, 0, 0)
-	label3.Text.SetPosition(0, label2.Text.X1.Y)
+	label3.AddShadow(1.5, 0, 0, 0)
 	label3.Text.SetColor(0.5, 0.5, 0.5, 1)
 	label3.OnClick = func(label *glmenu.Label, xPos, yPos float64) (err error) {
 		window.SetShouldClose(true)
@@ -77,7 +70,13 @@ func menuInit(window *glfw.Window) (err error) {
 		label.Text.AddScale(-menu.TextScaleRate)
 		return
 	}
-	label3.UpdateShadow(3, 0, 0, 0)
+
+	// simple centering of values
+	totalHeight := label.Text.X2.Y - label.Text.X1.Y + label2.Text.X2.Y - label2.Text.X1.Y + label2.Text.X2.Y - label2.Text.X1.Y
+	label.Text.SetPosition(0, totalHeight/2)
+	label.UpdateShadow(1.5, 0, 0, 0)
+	label3.Text.SetPosition(0, -totalHeight/2)
+	label3.UpdateShadow(1.5, 0, 0, 0)
 
 	return
 }
